4f88728139a30db96ccc4239a9b9dd32225bbde2
xmulyj <xmulyj@gmail.com>
2013-03-13 23:25:34 +0800

    add cpp files

---
+++ src/common/heap.cpp
@@ -0,0 +1,137 @@
+/*
+ * HeapSort.cpp
+ *
+ *  Created on: 2012-12-23
+ *      Author: LiuYongJin
+ */
+#include "heap.h"
+
+#include <string.h>
+#include <assert.h>
+
+#define INIT_CAPACITY 128
+
+Heap::Heap(ItemCompare cmp_func)
+{
+	assert(cmp_func != NULL);
+
+	m_items = (HeapItem**)malloc(sizeof(HeapItem*)*INIT_CAPACITY);
+	assert(m_items != NULL);
+
+	m_size = 0;
+	m_capacity = INIT_CAPACITY;
+	m_cmp_func = cmp_func;
+}
+
+Heap::~Heap()
+{
+	free(m_items);
+	m_items = NULL;
+}
+
+//插入元素.成功返回ture,失败返回false
+bool Heap::insert(HeapItem *item)
+{
+	assert(item != NULL);
+	if(m_size == m_capacity)
+		if(_expand_capacity() != 0)
+			return false;
+
+	item->index = m_size;
+	m_items[m_size++] = item;
+	_shift_up(item->index);
+	return true;
+}
+
+//删除堆元素.成功返回ture,失败返回false
+bool Heap::remove(HeapItem *item)
+{
+	assert(item != NULL);
+	if(item->index >= m_size)
+		return false;
+	HeapItem *temp = m_items[item->index] = m_items[--m_size];
+	temp->index = item->index;
+	int result = m_cmp_func(temp, item);
+	if(result == -1)
+		_shift_up(temp->index);
+	else if(result == 1)
+		_shift_down(temp->index);
+	return true;
+}
+
+//删除堆顶元素
+void Heap::pop()
+{
+	if(m_size <= 0)
+		return;
+	if(--m_size <= 0)
+		return;
+
+	m_items[0] = m_items[m_size];
+	m_items[0]->index = 0;
+	_shift_down(0);
+	return ;
+}
+
+//清除堆
+void Heap::clear(ItemDestroy des_func)
+{
+	if(des_func != NULL)
+		while(m_size > 0)
+			des_func(m_items[--m_size]);
+	m_size = 0;
+	if(m_capacity > INIT_CAPACITY)
+	{
+		m_capacity = INIT_CAPACITY;
+		m_items = (HeapItem **)realloc((void*)m_items, sizeof(HeapItem*)*m_capacity);
+	}
+}
+
+bool Heap::_expand_capacity()
+{
+	int new_capacity = m_capacity*2;
+	HeapItem **new_ptr =(HeapItem **)realloc((void*)m_items, sizeof(HeapItem*)*new_capacity);
+	if(new_ptr == NULL)
+		return false;
+	m_items = new_ptr;
+	m_capacity = new_capacity;
+	return true;
+}
+
+void Heap::_shift_up(uint32_t index)
+{
+	HeapItem *item = m_items[index];
+	assert(index == item->index);
+	int parent = (index-1)/2;
+	while(index>0 && parent>=0)
+	{
+		if(m_cmp_func(m_items[parent], item) != 1)    //parent不大于child
+			break;
+		m_items[index] = m_items[parent];
+		m_items[index]->index = index;
+		index = parent;
+		parent = (index-1)/2;
+	}
+	m_items[index] = item;
+	item->index = index;
+}
+
+void Heap::_shift_down(uint32_t index)
+{
+	HeapItem *item = m_items[index];
+	assert(index == item->index);
+	int child = index*2+1;
+	while(child < m_size)
+	{
+		if(child+1<m_size && m_cmp_func(m_items[child+1], m_items[child])==-1)  //右孩子更小
+			++child;
+		if(m_cmp_func(item, m_items[child]) != 1)    //比最小的孩子还小
+			break;
+		m_items[index] = m_items[child];
+		m_items[index]->index = index;
+		index = child;
+		child = index*2+1;
+	}
+	m_items[index] = item;
+	item->index = index;
+}
---
+++ src/common/iodemuxer.cpp
@@ -0,0 +1,126 @@
+/*
+ * iodemuxer.cpp
+ *
+ *  Created on: 2013-3-7
+ *      Author: LiuYongJin
+ */
+
+#include "iodemuxer.h"
+#include <sys/time.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <list>
+using std::list;
+
+#define WAIT_TIME    200    //io事件的等待时间取该值和最小超时时间的较小值
+
+typedef struct _timer_info
+{
+	HeapItem heap_item;
+	uint32_t timeout;
+	TimerHandler *handler;
+	uint64_t expire_time;    //超时时间点(ms)
+}TimerInfo;
+
+static int _timer_info_cmp(HeapItem *item0, HeapItem *item1)
+{
+	TimerInfo *timerinfo0 = (TimerInfo*)item0;
+	TimerInfo *timerinfo1 = (TimerInfo*)item1;
+	if(timerinfo0->expire_time < timerinfo1->expire_time)
+		return -1;
+	else if (timerinfo0->expire_time == timerinfo1->expire_time)
+		return 0;
+	else
+		return 1;
+}
+
+IODemuxer::IODemuxer()
+	:m_timer_heap(_timer_info_cmp)
+	,m_timerinfo_pool(sizeof(TimerInfo))
+	,m_exit(false)
+{
+}
+
+bool IODemuxer::add_timer(TimerHandler *handler, uint32_t timeout)
+{
+	assert(handler != NULL);
+
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	uint64_t now_time = tv.tv_sec*1000+tv.tv_usec/1000;
+
+	TimerInfo *timer_info;
+	if((timer_info=(TimerInfo*)m_timerinfo_pool.get()) == NULL)
+	{
+		return false;
+	}
+
+	timer_info->handler = handler;
+	timer_info->timeout = timeout;
+	timer_info->expire_time = now_time+timeout;
+	if(!m_timer_heap.insert((HeapItem*)timer_info))
+	{
+		m_timerinfo_pool.recycle((void*)timer_info);
+		return false;
+	}
+
+	return true;
+}
+
+bool IODemuxer::run_loop()
+{
+
+	uint64_t now_time;                //当前时间(单位ms)
+	uint32_t wait_time;               //io事件等待时间(单位ms)
+	list<TimerInfo*> timeout_list;    //超时时钟列表
+
+	m_exit = false;
+	while(!m_exit)
+	{
+		struct timeval tv;
+		gettimeofday(&tv, NULL);
+		now_time = tv.tv_sec*1000+tv.tv_usec/1000;
+
+		wait_time = WAIT_TIME;
+		//收集时钟超时事件,同时设置wait_time
+		timeout_list.clear();
+		TimerInfo *timer_info;
+		while((timer_info=(TimerInfo*)m_timer_heap.top()) != NULL)
+		{
+			if(timer_info->expire_time <= now_time)    //时钟超时
+			{
+				timeout_list.push_back(timer_info);
+				m_timer_heap.pop();
+				continue;
+			}
+
+			if((wait_time=timer_info->expire_time-now_time) > WAIT_TIME)    //取最小值
+				wait_time = WAIT_TIME;
+			break;
+		}
+
+		//处理io事件
+		dispatch_events(now_time, wait_time);
+
+		//处理发生的时钟超时事件
+		while(!timeout_list.empty())
+		{
+			TimerInfo *timer_info = *timeout_list.begin();
+			timeout_list.pop_front();
+
+			if(timer_info->handler->on_timer_timeout() == HANDLE_CONTINUE)    //间隔时钟
+			{
+				timer_info->expire_time = now_time+timer_info->timeout;
+				bool temp = m_timer_heap.insert((HeapItem*)timer_info);
+				assert(temp == true);
+			}
+			else
+			{
+				m_timerinfo_pool.recycle((void*)timer_info);
+			}
+		}
+	}
+
+	return true;
+}
