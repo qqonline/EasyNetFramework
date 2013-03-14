8c721bb2d9ac5cce04fbba3f100101ea30a71ee3
xmulyj <xmulyj@gmail.com>
2013-03-14 23:38:02 +0800

    bytebuffer

---
+++ include/common/bytebuffer.h
@@ -0,0 +1,109 @@
+/*
+ * bytebuffer.h
+ *
+ *  Created on: 2013-3-14
+ *      Author: LiuYongJin
+ */
+
+#ifndef _COMMON_BYTEBUFFER_H_
+#define _COMMON_BYTEBUFFER_H_
+
+#include <stdint.h>
+#define BF_INIT_CAPACITY 512
+
+class ByteBuffer
+{
+public:
+	ByteBuffer(uint32_t init_capacity=BF_INIT_CAPACITY);
+	~ByteBuffer();
+
+	//获取整个buffer
+	char* get_buffer(){return m_base;}
+	//获取总数据大小
+	uint32_t get_size(){return m_size;}
+
+	//从有效数据后面获取一个大小为size的buffer用于添加数据
+	char* get_append_buffer(uint32_t size);
+	//设置添加到buffer中的大小
+	void set_append_size(uint32_t size);
+
+	//返回可获取的数据大小
+	uint32_t get_fetch_size();
+	//获取有效数据的buffer用于获取数据,返回有效数据的size;没有数据返回NULL,size设置为0
+	char* get_fetch_buffer(uint32_t *size=NULL);
+	//设置从buffer中取出的数据大小;
+	void set_fetch_size(uint32_t size);
+public:
+	//添加大小为size的bytes数组到buffer后面
+	bool append_bytes(char *bytes, uint32_t size);
+	//添加字符串(不包含'\0')到buffer后面
+	bool append_str(char *str);
+	//添加32位整数i到buffer后面
+	bool append_int32(int32_t i);
+	//添加64位整数到buffer后面
+	bool append_int64(int64_t i);
+
+	//严格从buffer读出size字节到buf中.数据不足size字节返回false.
+	bool fetch_bytes(char *buf, uint32_t size);
+	//严格从buffer读出32位整数.数据不足size字节返回false.
+	bool fetch_int32(int32_t *i);
+	//严格从buffer读取64位整数.数据不足size字节返回false.
+	bool fetch_int64(int64_t *i);
+private:
+	char *m_base;
+	uint32_t m_capacity;
+	uint32_t m_size;
+	uint32_t m_fetch_pos;
+};
+
+inline
+void ByteBuffer::set_append_size(uint32_t size)
+{
+	assert(m_size+size <= m_capacity);
+	m_size += size;
+}
+
+inline
+void ByteBuffer::set_fetch_size(uint32_t size)
+{
+	assert(m_fetch_pos+size <= m_size);
+	m_fetch_pos += size;
+}
+
+inline
+bool ByteBuffer::append_str(char *str)
+{
+	return str==NULL?true:append_bytes(str, strlen(str));
+}
+
+inline
+bool ByteBuffer::append_int32(int32_t i)
+{
+	return append_bytes((char*)&i, sizeof(i));
+}
+
+inline
+bool ByteBuffer::append_int64(int64_t i)
+{
+	return append_bytes((char*)&i, sizeof(i));
+}
+
+inline
+uint32_t ByteBuffer::get_fetch_size()
+{
+	return m_fetch_pos<m_size?m_size-m_fetch_pos:0;
+}
+
+bool ByteBuffer::fetch_int32(int32_t *i)
+{
+	return i==NULL?false:fetch_bytes((char*)i, sizeof(int32_t));
+}
+
+bool ByteBuffer::fetch_int64(int64_t *i)
+{
+	return i==NULL?false:fetch_bytes((char*)i, sizeof(int64_t));
+}
+
+#endif //_COMMON_BYTEBUFFER_H_
+
+
---
+++ src/common/bytebuffer.cpp
@@ -0,0 +1,86 @@
+/*
+ * bytebuffer.cpp
+ *
+ *  Created on: 2013-3-14
+ *      Author: LiuYongJin
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <common/bytebuffer.h>
+
+ByteBuffer::ByteBuffer(uint32_t init_capacity/*=BF_INIT_CAPACITY*/)
+	:m_capacity(init_capacity)
+	,m_size(0)
+	,m_fetch_pos(0)
+{
+	m_base = (char*)calloc(m_capacity, 1);
+	assert(m_base != NULL);
+}
+
+ByteBuffer::~ByteBuffer()
+{
+	free(m_base);
+	m_base = NULL;
+	m_capacity = 0;
+	m_size = 0;
+}
+
+//从有效数据后面获取一个大小为size的buffer用于添加数据
+char* ByteBuffer::get_append_buffer(uint32_t size)
+{
+	assert(size > 0);
+	if(m_size+size > m_capacity)
+	{
+		uint32_t new_cap = m_capacity;
+		do{
+			new_cap <<= 1;
+		}while(m_size+size > new_cap);
+
+		char* new_base = (char*)realloc((void*)m_base, new_cap);
+		if(new_base == NULL)
+			return NULL;
+		m_base = new_base;
+		m_capacity = new_cap;
+	}
+	return m_base+m_size;
+}
+
+//获取有效数据的buffer用于获取数据,返回有效数据的size;
+char* ByteBuffer::get_fetch_buffer(uint32_t *size/*=NULL*/)
+{
+	if(size != NULL)
+		*size = 0;
+
+	if(m_fetch_pos >= m_size)
+		return NULL;
+	if(size != NULL)
+		*size = m_size-m_fetch_pos;
+	return m_base+m_fetch_pos;
+}
+
+bool ByteBuffer::append_bytes(char *bytes, uint32_t size)
+{
+	if(bytes==NULL || size==0)
+		return true;
+
+	char *temp = get_append_buffer(size);
+	if(temp == NULL)
+		return false;
+	memcpy(temp, bytes, size);
+	set_append_size(size);
+	return true;
+}
+
+bool ByteBuffer::fetch_bytes(char *buf, uint32_t size)
+{
+	assert(buf != NULL);
+	uint32_t len;
+	char *temp = get_fetch_buffer(&len);
+	if(temp==NULL || len<size)
+		return false;
+	memcpy(buf, temp, size);
+	return true;
+}
