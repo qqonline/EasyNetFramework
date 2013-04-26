542409d35e7ccec6c0e9d273687fef5823996c25
xmulyj <xmulyj@gmail.com>
2013-04-26 20:01:39 +0800

    KVProtocol

---
+++ include/common/KVProtocol.h
@@ -0,0 +1,53 @@
+/*
+ * KVProtocol.h
+ *
+ *  Created on: Apr 26, 2013
+ *      Author: LiuYongJin
+ */
+#ifndef _COMMON_KEY_VALUE_PROTOCOL_H_
+#define _COMMON_KEY_VALUE_PROTOCOL_H_
+
+#include <stdint.h>
+#include <map>
+using std::map;
+#include <string>
+using std::string;
+
+#include "Logger.h"
+
+namespace easynet
+{
+
+typedef map<uint16_t, uint8_t*> PosMap;
+class KVProtocol
+{
+public:
+	KVProtocol();
+	~KVProtocol();
+	bool Set(uint16_t key, int32_t val);
+	bool Set(uint16_t key, int64_t val);
+	bool Set(uint16_t key, int8_t *bytes, uint32_t size);
+	bool Set(uint16_t key, string str);
+
+	bool Get(uint16_t key, int32_t *val);
+	bool Get(uint16_t key, int64_t *val);
+	bool Get(uint16_t key, int8_t **bytes, uint32_t *size);
+	bool Get(uint16_t key, string *str);
+private:
+	uint32_t   m_Size;
+	uint32_t   m_Capacity;
+	int8_t    *m_Buffer;
+	PosMap     m_PosMap;
+
+	bool _Set(uint16_t key, uint16_t type, int8_t *bytes, uint32_t size);
+	bool _Get(uint16_t key, uint16_t type, int8_t **bytes, uint32_t *size);
+	bool _ExpandCapacity(uint32_t need_size);
+private:
+	DECL_LOGGER(logger);
+};
+
+}//namespace
+#endif //_COMMON_KEY_VALUE_PROTOCOL_H_
+
+
+
---
+++ src/common/KVProtocol.cpp
@@ -0,0 +1,165 @@
+/*
+ * KVProtocol.cpp
+ *
+ *  Created on: Apr 26, 2013
+ *      Author: LiuYongJin
+ */
+#include <stdlib.h>
+#include <assert.h>
+
+#include "KVProtocol.h"
+
+namespace easynet
+{
+
+IMPL_LOGGER(KVProtocol, logger);
+
+#define KV_CAPACITY   1024    //初始化大小为1k
+
+#define TYPE_I32   0
+#define TYPE_I64   1
+#define TYPE_BYTES 2
+#define TYPE_BP0   2    //byte,填充0字节
+#define TYPE_BP1   3    //byte,填充1字节
+#define TYPE_BP2   4    //byte,填充2字节
+#define TYPE_BP3   5    //byte,填充3字节
+
+
+KVProtocol::KVProtocol()
+	:m_Size(0)
+	,m_Capacity(KV_CAPACITY)
+{
+	m_Buffer = (uint8_t*)calloc(m_Capacity, 1);
+	assert(m_Buffer != NULL);
+}
+
+bool KVProtocol::_Set(uint16_t key, uint16_t type, int8_t *bytes, uint32_t size)
+{
+	uint32_t len = sizeof(uint16_t)*2+size;
+	if(type == TYPE_BYTES)
+	{
+		len += sizeof(uint32_t);       //for size;
+		type += len%sizeof(uint32_t);
+		len += len%sizeof(uint32_t);   //考虑地址对齐
+	}
+
+	if(m_Size+len>m_Capacity && !_ExpandCapacity(m_Size+len))
+		return false;
+
+	char *ptr = m_Buffer+m_Size;
+	*(uint16_t*)ptr = key;
+	ptr += sizeof(uint16_t);
+	*(uint16_t*)ptr = type;
+	ptr += sizeof(uint16_t);
+	if(type >= TYPE_BP0)
+	{
+		*(uint32_t*)ptr = size;
+		ptr += sizeof(uint32_t);
+	}
+	memcpy((void*)ptr, (void*)bytes, size);
+
+	return true;
+}
+
+bool KVProtocol::_ExpandCapacity(uint32_t need_size)
+{
+	uint32_t capacity = (1+need_size/KV_CAPACITY)*KV_CAPACITY;
+	if(capacity-need_size < KV_CAPACITY/2)
+		capacity += KV_CAPACITY;
+
+	uint8_t *temp = (uint8_t*)realloc(m_Buffer, capacity);
+	if(temp == NULL)
+	{
+		LOG_ERROR(logger, "kvprotocol out of memory");
+		return false;
+	}
+	m_Buffer = temp;
+	m_Capacity = capacity;
+	return true;
+}
+
+bool KVProtocol::Set(uint16_t key, int32_t val)
+{
+	return _Set(key, TYPE_I32, (int8_t*)&val, sizeof(int32_t));
+}
+
+bool KVProtocol::Set(uint16_t key, int64_t val)
+{
+	return _Set(key, TYPE_I64, (int8_t*)&val, sizeof(int64_t));
+}
+
+bool KVProtocol::Set(uint16_t key, int8_t *bytes, uint32_t size)
+{
+	if(bytes==NULL || size==0)
+		return false;
+	return _Set(key, TYPE_BYTES, bytes, size);
+}
+
+bool KVProtocol::Set(uint16_t key, string str)
+{
+	if(str.size() == 0)
+		return false;
+	return _Set(key, TYPE_BYTES, (int8_t*)str.c_str(), str.size());
+}
+
+bool KVProtocol::Get(uint16_t key, int32_t *val)
+{
+	if(val == NULL)
+		return false;
+	return _Get(key, TYPE_I32, (int8_t**)val, NULL);
+}
+
+bool KVProtocol::Get(uint16_t key, int64_t *val)
+{
+	if(val == NULL)
+		return false;
+	return _Get(key, TYPE_I64, (int8_t**)val, NULL);
+}
+
+bool KVProtocol::Get(uint16_t key, int8_t **bytes, uint32_t *size)
+{
+	if(bytes == NULL || size==NULL)
+		return false;
+	return _Get(key, TYPE_BYTES, bytes, size);
+}
+
+bool KVProtocol::Get(uint16_t key, string *str)
+{
+	if(str == NULL)
+		return false;
+	int8_t *bytes = NULL;
+	uint32_t size = 0;
+	if(!_Get(key, TYPE_BYTES, &bytes, &size))
+		return false;
+	str->assign((const char*)bytes, size);
+	return true;
+}
+
+bool KVProtocol::_Get(uint16_t key, uint16_t type, int8_t **bytes, uint32_t *size)
+{
+	PosMap::iterator it = m_PosMap.find(key);
+	if(it == m_PosMap.end())
+		return false;
+	uint8_t *ptr = it->second;
+	ptr += sizeof(uint16_t);
+	uint16_t rel_type = *(uint16_t*)ptr;
+	ptr += sizeof(uint16_t);
+	if(rel_type >= TYPE_BP0)
+	{
+		rel_type = TYPE_BYTES;
+		*size = *(uint32_t)ptr;
+		ptr += sizeof(uint32_t);
+	}
+
+	if(type != rel_type)
+		return false;
+	if(type == TYPE_I32)
+		memcpy((void*)bytes, ptr, sizeof(uint32_t));
+	else if(type == TYPE_I64)
+		memcpy((void*)bytes, ptr, sizeof(uint32_t));
+	else
+		*bytes = ptr;
+	return true;
+}
+
+}//namespace
