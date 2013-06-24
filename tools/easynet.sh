e7544a21348649d2baa6ce912063420236b62968
xmulyj <xmulyj@gmail.com>
2013-06-24 16:16:02 +0800

    easynet

--- tools/easynet.sh
+++ tools/easynet.sh
@@ -7,8 +7,9 @@ AUTHOR=`whoami`
 Usage()
 {
 	echo -e "easynet -a ClassName [-m]\n"
-	echo "  -a : Create Application Instance."
+	echo "  -a : Generate Application Instance Class."
 	echo "  -m : Optional. gen server main framework."
+	echo "  -p : Generate ProtocolFactory Class."
 }
 
 function GenAppInstance()
@@ -19,7 +20,7 @@ function GenAppInstance()
 
 	if [ "$Subfix" == "h" ];then
 		if [ -f "$ClassName.h" ];then
-			echo "ERROR: target file already exists: $ClassName.h...exit"
+			echo "ERROR: target file already exists: $ClassName.h ... [exit]"
 			exit
 		fi
 
@@ -27,7 +28,7 @@ function GenAppInstance()
 		Filename=$ClassName.h
 	elif [ "$Subfix" == "cpp" ];then
 		if [ -f "$ClassName.cpp" ];then
-			echo "ERROR: target file already exists: $ClassName.cpp...exit"
+			echo "ERROR: target file already exists: $ClassName.cpp ... [exit]"
 			exit
 		fi
 
@@ -46,7 +47,7 @@ function GenAppMain()
 	local ClassName=$1
 
 	if [ -f "${ClassName}Main.cpp" ];then
-		echo "ERROR: target file already exists: ${ClassName}Main.cpp...exit"
+		echo "ERROR: target file already exists: ${ClassName}Main.cpp ... [exit]"
 		exit
 	fi
 
@@ -55,6 +56,29 @@ function GenAppMain()
 	sed -i "s/_#CreateDate#_/${DATE}/g" ${ClassName}Main.cpp
 	sed -i "s/_#AuthorName#_/${AUTHOR}/g" ${ClassName}Main.cpp
 }
+
+function GenProtocolFactory()
+{
+	local ClassName=$1
+
+	### h
+	cp ${TEMPLATEDIR}/TemplateProtocolFactory.th $ClassName.h
+	sed -i "s/TemplateProtocolFactory/$ClassName/g" $ClassName.h
+
+	local UP_ClassName=$(echo $ClassName|tr '[a-z]' '[A-Z]')
+	sed -i "s/TEMPLATEPROTOCOLFACTORY/${UP_ClassName}/g" $ClassName.h
+	sed -i "s/_#CreateDate#_/${DATE}/g" $ClassName.h
+	sed -i "s/_#AuthorName#_/${AUTHOR}/g" $ClassName.h
+
+	### cpp
+	cp ${TEMPLATEDIR}/TemplateProtocolFactory.tpp $ClassName.cpp
+	sed -i "s/TemplateProtocolFactory/$ClassName/g" $ClassName.cpp
+	sed -i "s/_#CreateDate#_/${DATE}/g" $ClassName.cpp
+	sed -i "s/_#AuthorName#_/${AUTHOR}/g" $ClassName.cpp	
+}
+
+
+##########################  main  ##########################
 
 if [ $# -lt 2 ];then
 	Usage;
@@ -66,17 +90,31 @@ if [ "$1" == "-a" ];then
 		Usage;
 		exit;	
 	fi
-	echo "Gen Application instance class file: $2.h..."
+	echo "Generate Application instance class file: $2.h ..."
 	GenAppInstance $2 h;
 
-	echo "Gen Application instance class file: $2.cpp..."
+	echo "Generate Application instance class file: $2.cpp ..."
 	GenAppInstance $2 cpp;
 	
 	if [ $# -gt 2 ] && [ "$3" == "-m" ];then
-		echo "Gen ApplicationMain class file: $2Main.cpp..."
+		echo "Generate ApplicationMain class file: $2Main.cpp ..."
 		GenAppMain $2;
 	fi
 
-	echo -e "\nCompile Using:\n\t-I/usr/include/easynet -leasynet -lpthread -llog4cplus"
+	echo -e "\nCompile Using:\n\t-I/usr/include/easynet -leasynet -lpthread -llog4cplus\n\nSee temp makefile: Makefile.tmp"
+	echo "INCLUDE=-I/usr/include/easynet"> Makefile.tmp
+	echo "LIBS=-leasynet -lpthread -llog4cplus" >> Makefile.tmp
+elif [ "$1" == "-p" ];then
+	echo -e "Generate ProtocolFactory class file:\n\t$2.h ...\n\t$2.cpp ..."
+	if [ -f "$2.h" ];then
+		echo "ERROR: ProtocolFactory class file: $2.h already exists ... [exit]"
+		exit;
+	elif [ -f "$2.cpp" ];then
+		echo "ERROR: ProtocolFactory class file: $2.cpp already exists ... [exit]"
+		exit;
+	fi
+	GenProtocolFactory $2;
+	
+	echo "Generate ProtocolFactory ... [OK]"
 fi
 
