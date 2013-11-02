54e1723141d901535cdcde12833c63f5ee8db912
jack <jack@localhost.localdomain>
2013-11-02 13:41:53 +0800

    bytebuffer

--- src/common/ByteBuffer.h
+++ src/common/ByteBuffer.h
@@ -6,7 +6,7 @@
  */
 #ifndef _COMMON_BYTEBUFFER_H_TMP
 #define _COMMON_BYTEBUFFER_H_TMP
-
+#include <assert.h>
 #include "IMemory.h"
 
 namespace easynet
@@ -45,6 +45,28 @@ public:
 	char *Buffer;      //数据缓冲区
 	uint32_t Capacity; //缓冲区的容量
 	uint32_t Size;     //缓冲区数据大小
+
+public:
+	uint32_t Offset;   //输入的偏移位置
+	void Clear(){Size=0;Offset=0;}
+	ByteBuffer& operator <<(int8_t value);
+	ByteBuffer& operator <<(uint8_t value);
+	ByteBuffer& operator <<(int16_t value);
+	ByteBuffer& operator <<(uint16_t value);
+	ByteBuffer& operator <<(int32_t value);
+	ByteBuffer& operator <<(uint32_t value);
+	ByteBuffer& operator <<(int64_t value);
+	ByteBuffer& operator <<(uint64_t value);
+
+	ByteBuffer& operator >>(int8_t &value);
+	ByteBuffer& operator >>(uint8_t &value);
+	ByteBuffer& operator >>(int16_t &value);
+	ByteBuffer& operator >>(uint16_t &value);
+	ByteBuffer& operator >>(int32_t &value);
+	ByteBuffer& operator >>(uint32_t &value);
+	ByteBuffer& operator >>(int64_t &value);
+	ByteBuffer& operator >>(uint64_t &value);
+
 private:
 	ByteBuffer(const ByteBuffer &buffer){}
 	ByteBuffer& operator=(ByteBuffer &buffer){return *this;}
@@ -54,6 +76,137 @@ private:
 	static SystemMemory m_SysMemory;
 	IMemory *m_Memory;   //对象的内存分配器
 };
+
+
+#define SetValue(T,V) do{   \
+ CheckSize(sizeof(T));      \
+ char *temp = Buffer+Size;  \
+ *((T*)temp) = V;           \
+ Size += sizeof(T);         \
+}while(0)
+
+inline
+ByteBuffer& ByteBuffer::operator <<(int8_t value)
+{
+	SetValue(int8_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator <<(uint8_t value)
+{
+	SetValue(uint8_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator <<(int16_t value)
+{
+	SetValue(int16_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator <<(uint16_t value)
+{
+	SetValue(uint16_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator <<(int32_t value)
+{
+	SetValue(int32_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator <<(uint32_t value)
+{
+	SetValue(uint32_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator <<(int64_t value)
+{
+	SetValue(int64_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator <<(uint64_t value)
+{
+	SetValue(uint64_t, value);
+	return *this;
+}
+
+
+#define GetValue(T,V) do {       \
+ assert(Offset+sizeof(T)<=Size); \
+ char *temp = Buffer+Offset;     \
+ V = *(T*)temp;                  \
+ Offset += sizeof(T);            \
+}while(0)
+
+inline
+ByteBuffer& ByteBuffer::operator >>(int8_t &value)
+{
+	GetValue(int8_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator >>(uint8_t &value)
+{
+	GetValue(uint8_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator >>(int16_t &value)
+{
+	GetValue(int16_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator >>(uint16_t &value)
+{
+	GetValue(uint16_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator >>(int32_t &value)
+{
+	GetValue(int32_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator >>(uint32_t &value)
+{
+	GetValue(uint32_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator >>(int64_t &value)
+{
+	GetValue(int64_t, value);
+	return *this;
+}
+
+inline
+ByteBuffer& ByteBuffer::operator >>(uint64_t &value)
+{
+	GetValue(uint64_t, value);
+	return *this;
+}
+
+#undef SetValue
+#undef GetValue
 
 }//namespace
 #endif //_COMMON_BYTEBUFFER_H_TMP
